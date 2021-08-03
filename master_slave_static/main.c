6f8ed9b22ffe5ee1dca5457af5c527a1db6d4875
omri <t-omrimalka@microsoft.com>
2021-08-03 22:41:09 +0300

    Update solution

--- master_slave_static/main.c
+++ master_slave_static/main.c
@@ -2,90 +2,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-#include <omp.h>
-
-enum ranks{ROOT,M=8000,N=800};
-
-int generateNumbers(int* arr, int size);
-
-int findMax(int* arr, int size);
-
-int main(int argc, char *argv[])
-{
-
-    int my_rank, num_procs,*arr,my_arr[N],arr_size,local_max,global_max=0,jobs_done=0,temp_max;
-    double start;
-
-    MPI_Init(&argc, &argv);
-
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-
-    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
-    arr_size=num_procs*M*N;
-
-    if(my_rank == ROOT)
-    {
-        arr = (int*)malloc(arr_size*sizeof(int));
-        generateNumbers(arr,arr_size);
-        start = MPI_Wtime();
-    }
-
-    while(jobs_done <arr_size)
-    {
-        MPI_Scatter(arr+jobs_done,N,MPI_INT,my_arr,N,MPI_INT,ROOT,MPI_COMM_WORLD);
-
-        local_max = findMax(my_arr,N);
-
-        MPI_Reduce(&local_max,&temp_max,1,MPI_INT,MPI_MAX,ROOT,MPI_COMM_WORLD);
-
-        if(my_rank == ROOT)
-        {
-            if(temp_max > global_max)
-                global_max = temp_max;
-        }
-        jobs_done+= N*num_procs;
-    }
-    if(my_rank == ROOT)
-    {
-        printf("Parallel: The global max is :%d\n",global_max);
-        printf("Time %lf\n",MPI_Wtime()-start);
-        //control
-        start = MPI_Wtime();
-        global_max = findMax(arr,arr_size);
-        printf("Serial: The global max is :%d\n",global_max);
-        printf("Time %lf\n",MPI_Wtime()-start);
-    }
+#include <math.h>
+enum ranks{ROOT,M=200,N=105};
 
 
-    
-    
-    MPI_Finalize();
-    return 0;
-}
-
-
-
-int generateNumbers(int* arr, int size)
+void generateNumbers(int* arr, int size)
 {
 	int i;
 
 	srand(time(NULL));
 	for (i = 0; i < size; i++)
 	{
-		arr[i] = rand() % 100000001;
+		arr[i] = rand() % 10000000;
+        
 	}
 }
 
-int findMax(int* arr, int size)
+int main(int argc, char *argv[])
 {
-	int i, max = -1;
 
-	for (i = 0; i < size; i++)
-	{
-		if(arr[i]>max)
-            max =  arr[i];
-	}
+    int my_rank, num_procs,arrsize, my_max, Max,i;
+    int *arr, *work_arr;
+    
+    MPI_Init(&argc, &argv);
 
-	return max;
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs); 
+
+    arrsize = M*N*num_procs;
+
+    if(my_rank == ROOT)
+    {
+        arr = (int*)malloc(sizeof(int)*arrsize);
+        generateNumbers(arr,arrsize);
+    }
+
+    work_arr =  (int*)malloc(sizeof(int)*arrsize/num_procs);
+    MPI_Scatter(arr,arrsize/num_procs,MPI_INT,work_arr,arrsize/num_procs,MPI_INT,ROOT,MPI_COMM_WORLD);
+    my_max = work_arr[0];
+    for(i=0;i<arrsize/num_procs;i++)
+    {
+        if(work_arr[i] > my_max)
+            my_max = work_arr[i];
+    }
+    MPI_Reduce(&my_max,&Max,1,MPI_INT,MPI_MAX,ROOT,MPI_COMM_WORLD);
+
+    if(my_rank == ROOT)
+    {
+        printf("Parallel Max %d\n",Max);
+        my_max = arr[0];
+        for(i=0;i<arrsize;i++)
+        {
+            if(arr[i] > my_max)
+                my_max = arr[i];
+        }
+        printf("Sequential Max %d\n",my_max);
+        free(arr);
+    }
+    free(work_arr);
+   
+
+    MPI_Finalize();
+    return 0;
 }
--- master_slave_static/readme.md
+++ master_slave_static/readme.md
@@ -1,3 +1,4 @@
-# Dynamic
+# Static master slave
 
-
+1. Run `$ make`.
+2. `$ make run`.
